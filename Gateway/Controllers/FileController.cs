bfaedb816e0f0d8dacd95082c3d3cc90e8a26b0d
EnumutsubiKami <lap.pt215076@sis.hust.edu.vn>
2024-11-22 20:48:38 +0700

    init backend

---
+++ Gateway/Controllers/FileController.cs
@@ -0,0 +1,44 @@
+﻿using Gateway.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gateway.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public  async Task<IActionResult> GetImageFromServer([FromQuery] string name)
+        {
+            var file = FileHelper.getFile(name);
+            if (file == null)
+            {
+                return NotFound(new
+                {
+                    Content = "Invalid file name",
+                    Message = "File name not found or not exist"
+                });
+            }
+            return File(file, "image/jpeg");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> PostImageToServer(IFormFile file) 
+        {
+            var newFile = FileHelper.saveFile(file);
+            if (string.IsNullOrEmpty(newFile))
+            {
+                return BadRequest(new {
+                    Content = "Invalid format file",
+                    Message = "File is not JPEG or image format"
+                });
+            }
+            return Ok(new
+            {
+                Data = newFile,
+                Message = "Upload file successfully"
+            });
+        }
+    }
+}
---
+++ Gateway/Helpers/FileHelper.cs
@@ -0,0 +1,54 @@
+﻿namespace Gateway.Helpers
+{
+    public static class FileHelper
+    {
+        public static string saveFile(IFormFile file)
+        {
+            try
+            {
+                var fileName = file.FileName;
+                var extensionName = Path.GetExtension(fileName);
+                while (true)
+                {
+                    string newFilename = Guid.NewGuid().ToString() + extensionName;
+                    fileName = Path.Combine(Directory.GetCurrentDirectory(), "Resources", newFilename);
+                    if (File.Exists(fileName))
+                    {
+                        continue;
+                    }
+                    using (var stream = new FileStream(fileName, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    return newFilename;
+                }
+            }
+            catch (Exception e)
+            {
+                return string.Empty;
+            }
+        }
+
+        public static FileStream getFile(string fileName)
+        {
+            var imagePath = Path.Combine(Directory.GetCurrentDirectory(), "Resources", fileName);
+            if (!File.Exists(imagePath)) return null;
+            return File.OpenRead(imagePath);
+        }
+
+        public static bool removeFile(string fileName)
+        {
+            try
+            {
+                var file = Path.Combine(Directory.GetCurrentDirectory(), "Resources", fileName);
+                if (!File.Exists(file)) return false;
+                File.Delete(file);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
